6fb8c6a23b95e5d66946a37eb59dd5068b6eec96
Dennys Wong <freewheel1998@hotmail.com>
2025-12-04 21:10:11 +0100

    Temmplate + info complete

--- Tema 6 - Modelos de inteligencia artificial/src/blocks/AvatarWithRating/avatarWithRating.ts
+++
@@ -1,9 +0,0 @@
-import type { AvatarProps, RatingProps } from 'webcoreui/astro'
-
-export type AvatarWithRatingProps = {
-    avatar: AvatarProps
-    rating: RatingProps
-    text?: string
-    reverse?: boolean
-    className?: string
-}
